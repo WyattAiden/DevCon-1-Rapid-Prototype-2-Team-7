96daf8ea929a9e5cb7a62cf3c30bdec8775782a9
That-Cat-Vivo <eman3001@yahoo.com>
2025-10-01 14:21:00 -0400

    rename gameDirector update playerController

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    //access rigid bodies
+    public Rigidbody2D playerRB2D1;
+    public Rigidbody2D playerRB2D2;
+
+    //Movement
+    public float moveSpeed;
+    private Vector2 player1Location;
+    private Vector2 player2Location;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,17 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Track player locations
+        player1Location = new Vector2(playerRB2D1.position.x, playerRB2D1.position.y);
+        player2Location = new Vector2(playerRB2D2.position.x, playerRB2D2.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        //Player1 Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
     }
 }
